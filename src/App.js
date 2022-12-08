c630f0121dd97019fa9bf361fd28176bce8c5ca4
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-08 12:36:22 +0530

    learning useeffect hook l

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [toggle, setToggle] = useState(false)
+  const clickHandler = () => {
+    setToggle(!toggle)
+  }
+
+  useEffect(() => {
+    document.title = toggle
+      ? 'Welcome to My Github account'
+      : 'Using the useEfect hook'
+  }, [toggle])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Using the useEffect hook</h1>
+      <button onClick={clickHandler}>
+        Toggle message
+        {toggle && <h2>Welcome to My Github account</h2>}
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
